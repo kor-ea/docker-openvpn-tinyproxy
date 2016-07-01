2e60cdcc0ccacbb2667e9dbe7a68d3a5eac840b7
kor-ea <evgeny@kornyushin.com>
2016-07-01 21:19:04 +0300

    Update startdocker.sh

--- startdocker.sh
+++ startdocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 2000 >> /proc/sys/kernel/keys/root_maxkeys; echo 200000 >> /proc/sys/kernel/keys/root_maxbytes
+echo 100000 >> /proc/sys/kernel/keys/root_maxkeys; echo 2500000 >> /proc/sys/kernel/keys/root_maxbytes
 docker-compose build
 for f in configs/*.ovpn; do docker-compose run -d --rm vpn $f; done
 docker ps -a | grep 'Exited' | awk '{print $1}' | xargs --no-run-if-empty docker rm
