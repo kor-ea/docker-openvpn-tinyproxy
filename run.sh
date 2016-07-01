469bc6bf5cb5981d0aedb87c90aa7c1df03678b9
root <evgeny@kornyushin.com>
2016-07-01 05:16:07 -0400

    timeouts added

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 echo "---starting tinyproxy"
 tinyproxy -c /etc/openvpn/tinyproxy.conf 
 echo "---starting openvpn with config $1"
-openvpn $1 
+openvpn --config $1 --connect-retry-max 1 --tls-exit --ping 5 --ping-exit 10 --connect-timeout 5
