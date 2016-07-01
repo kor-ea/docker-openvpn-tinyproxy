de4d1162a5d0042083375a2cd91ef86605a62530
root <evgeny@kornyushin.com>
2016-07-01 07:17:39 -0400

    remove stopped

--- removestopped.sh
+++ removestopped.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker-compose build
-for f in configs/*.ovpn; do docker-compose run -d --rm vpn $f; done
 docker ps -a | grep 'Exited' | awk '{print $1}' | xargs --no-run-if-empty docker rm
